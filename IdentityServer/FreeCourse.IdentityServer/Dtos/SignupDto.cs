d753731ac8c5a60f177f7e220dee3531f7778f29
Nicat Mirzayev <nicat.mirzayev@setclapp.com>
2022-11-13 23:20:14 +0400

    #PhotoController

---
+++ IdentityServer/FreeCourse.IdentityServer/Dtos/SignupDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FreeCourse.IdentityServer.Dtos
+{
+    public class SignUpDto
+    {
+        [Required(ErrorMessage ="İstifadəçi adı daxil edin")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "E-poçt daxil edin")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Şifrə daxil edin")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Şəhər daxil edin")]
+        public string City { get; set; }
+    }
+}
--- Services/Catalog/FreeCourse.Services.Catalog/Program.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Program.cs
@@ -1,16 +1,28 @@
 using FreeCourse.Services.Catalog.Mapping;
 using FreeCourse.Services.Catalog.Services;
 using FreeCourse.Services.Catalog.Settings;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(x
+    =>
+{
+    x.Authority = builder.Configuration["IdentityServerUrl"];
+    x.Audience = "resource_catalog";
+    x.RequireHttpsMetadata = false;
+});
 // Add services to the container.
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<ICourseService, CourseService>();
 builder.Services.AddAutoMapper(typeof(GeneralMapper));
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(x =>
+{
+    x.Filters.Add(new AuthorizeFilter());
+});
 
 builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
 builder.Services.AddSingleton<IDatabaseSettings>(sp =>
@@ -21,6 +33,8 @@ builder.Services.AddSingleton<IDatabaseSettings>(sp =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -29,6 +43,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseAuthentication();
 
 app.UseAuthorization();
 
